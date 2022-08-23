66defadc3f454a4aceb5fd6ffdb742ffc69d5e61
learncodingbyvg <59496579+goelvaibhav105@users.noreply.github.com>
2022-08-23 19:00:29 +0530

    header file change

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>THIS IS MY HEADER COM. Test</h1>
-      </div>
+      <div>Header Change</div>
     )
   }
 }
