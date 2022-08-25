6493e400198428aca847c0bbe91c143ad461e56b
learncodingbyvg <59496579+goelvaibhav105@users.noreply.github.com>
2022-08-25 13:32:34 +0530

    React-router-dom Tutorial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import About from './components/About/About'
+import NavbarComp from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
+    <div className="App">  
+    <NavbarComp/>
+     <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/About" element={<About />} />
+    </Routes>
     </div>
   );
 }
